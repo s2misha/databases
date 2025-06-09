76acfd53040611b2aefaef20a66105e70413c708
s2misha <s2misha4321@gmail.com>
2025-06-09 11:03:26 -0500

    Update ejemplo1.sql
    sgsdgsdg

--- ejemplo1.sql
+++ ejemplo1.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS cliente;
 CREATE TABLE cliente 
  (
   id                         VARCHAR(3)  NOT NULL,
-  nombre                     VARCHAR(20) NOT NULL,
+  nombre                     VARCHAR(50) NOT NULL,
   telefono                   VARCHAR(20) NOT NULL,
   direccion                  VARCHAR(20),
   ciudad                     VARCHAR(20),
